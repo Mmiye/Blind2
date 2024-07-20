007ff441fa5f7aa17c36c37dcbc5e9e6550fe80d
Mmiye <158499826+Mmiye@users.noreply.github.com>
2024-07-21 00:32:35 +0200

    Update animeEds.js

--- src/js/data/2024-01-31/misc/animeEds.js
+++ src/js/data/2024-01-31/misc/animeEds.js
@@ -1,127 +1,7 @@
 export const animeEndings = [
   {
-    name: "The iDOLM@STER Million Live!01 - \"Akai Sekai ga Kieru Koro\" by Kido Ibuki, Oozeki Eri, Kondo Yui, Abe Rika & Hirayama Emi",
-    videoId: "0MgtK30HFrM",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "ED"
-      ]
-    }
-  },
-  {
-    name: "The iDOLM@STER Million Live!02 - \"Alive Facto\"r by Chihaya Kisaragi and Shizuka Mogami",
-    videoId: "efXIu65q9W0",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "ED"
-      ]
-    }
-  },
-  {
-    name: "The iDOLM@STER Cinderella Girls01 - \"always\" by Saori Hayami",
-    videoId: "TrVvFkGrDeg",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "ED"
-      ]
-    }
-  },
-  {
-    name: "The iDOLM@STER Million Live!03 - \"Big Bang's Volleyball!!!!!\" by Reina Ueda ,Yukiyo Fujii, Yuri Komagata, Rie Suegara, Yui Watanabe",
-    videoId: "Xs-mB6nFMV4",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "ED"
-      ]
-    }
-  },
-  {
-    name: "iDOLM@STER Xenoglossia - \"Binetsu S.O.S!!\" by Miyuki Hashimoto",
-    videoId: "jeagPlq9oAI",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "ED"
-      ]
-    }
-  },
-  {
-    name: "The iDOLM@STER Shiny Colors01 - \"BRIGHTEST WHITE\" by illumination STARS",
-    videoId: "5LVl_y1kuy8",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "ED"
-      ]
-    }
-  },
-  {
-    name: "THE iDOLM@STER Million Live!04 - \"Do the IDOL!! \uff5eKingashinnen Remix\uff5e\" by Koiwai Kotori, Kahara Yuu, Itou Miku, Saitou Yuka, Machico",
-    videoId: "vNySX8InPqQ",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "ED"
-      ]
-    }
-  },
-  {
-    name: "THE iDOLM@STER Shiny Colors02 - \"Dye the sky\" by Shiny Colors",
-    videoId: "jt0BDjLDPRY",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "ED"
-      ]
-    }
-  },
-  {
-    name: "THE iDOLM@STER Million Live!05 - \"Harmonics\" by D/Zeal",
-    videoId: "loHyF-mpJ0k",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "ED"
-      ]
-    }
-  },
-  {
-    name: "The iDOLM@STER Shiny Colors03 - \"Hoshi wo Mezashite\" by Yuki Tanaka",
-    videoId: "EzjX79cuHz8",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "ED"
-      ]
-    }
-  },
-  {
-    name: "The iDOLM@STER Million Live!06 - \"Hummingbird\" by Arisa Kouri",
-    videoId: "4KaX4It9mjw",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "ED"
-      ]
-    }
-  },
-  {
-    name: "Ijigen Fes IDOLM@STER\u2605\u2665Love Live! Uta Gassen - \"Ijigen\u2605\u2665BIGBANG\" by Ayaka Ohashi, Azusa Tadakoro, Karin Isobe, Anju Inami, Aguri Oonishi, Sayuri Date, Nozomi Nirei",
-    videoId: "3p7__uAHhdQ",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "ED"
-      ]
-    }
-  },
-  {
-    name: "The iDOLM@STER01 - \"Inspire\" by Asami Imai & Chiaki Takahashi",
-    videoId: "https://files.catbox.moe/ckuwgk.mp3",
+    name: "1 -  ",
+    videoId: "https://files.catbox.moe/c6i6en.mp3",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
@@ -130,88 +10,8 @@ export const animeEndings = [
     }
   },
   {
-    name: "The iDOLM@STER Shiny Colors04 - \"Itsudatte Bokura wa\" by noctchill",
-    videoId: "NAWIdRaCIzs",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "ED"
-      ]
-    }
-  },
-  {
-    name: "Gakuen iDOLM@STER - \"Ivy\" by Tsumiki and Nao Ojika",
-    videoId: "LATlgizqmPs",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "ED"
-      ]
-    }
-  },
-  {
-    name: "THE IDOLM@STER CINDERELLA GIRLS STARLIGHT MASTER COLLABORATION! - \"Jubilee\" by Hoshimachi Suisei and Kaede Takagaki",
-    videoId: "3mo2EwFZ0p8",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "ED"
-      ]
-    }
-  },
-  {
-    name: "The iDOLM@STER Shiny Colors05 - \"Kamisama wa Shinda, tte\" by Rina Kawaguchi",
-    videoId: "Xo5VNtvFmt8",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "ED"
-      ]
-    }
-  },
-  {
-    name: "The iDOLM@STER Million Live!07 - \"Last Actress\" by Risa Taneda, Keiko Watanabe, Minami Takahashi, Rika Abe & Saki Minami",
-    videoId: "bFlVMG8NWQE",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "ED"
-      ]
-    }
-  },
-  {
-    name: "The iDOLM@STER02 - \"Marionette no Kokoro\" by Akiko Hasegawa",
-    videoId: "HNsSnrxIsRw",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "ED"
-      ]
-    }
-  },
-  {
-    name: "The iDOLM@STER Cinderella Girls02 - \"Mata Kimi ni Koishiteru\" by Rika Tachibana",
-    videoId: "yqpSUc-RFVg",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "ED"
-      ]
-    }
-  },
-  {
-    name: "The iDOLM@STER Cinderella Girls03 - \"Myself\" by Haruka Chisuga",
-    videoId: "XLOaHFqbH_Q",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "ED"
-      ]
-    }
-  },
-  {
-    name: "The iDOLM@STER03 - \"Nemuri Hime\" by Asami Imai",
-    videoId: "https://ladist1.catbox.video/dxp7sc.webm",
+    name: "2 -  ",
+    videoId: "https://files.catbox.moe/abmvmz.mp3",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
@@ -220,68 +20,8 @@ export const animeEndings = [
     }
   },
   {
-    name: "The iDOLM@STER Cinderella Girls04 - \"Pandemic Alone (Live Version)\" by Syoko Hoshi (Matsuda Satsumi)",
-    videoId: "b2oygcmrsZs",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "ED"
-      ]
-    }
-  },
-  {
-    name: "The iDOLM@STER Cinderella Girls05 - \"Pretty Liar\" by Mysterious Eyes",
-    videoId: "mRIfqUClWMw",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "ED"
-      ]
-    }
-  },
-  {
-    name: "The iDOLM@STER Shiny Colors06 - \"Setsu Getsu Fu Ka\" by Mizuki Yuina",
-    videoId: "wYRZ7sFzOtQ",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "ED"
-      ]
-    }
-  },
-  {
-    name: "The iDOLM@STER Million Live!08 - \"Sister\" by Kotori Koiwai",
-    videoId: "vyBtQ2uYcaY",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "ED"
-      ]
-    }
-  },
-  {
-    name: "The iDOLM@STER Million Live!09 - \"Souzou wa Hajimari no Kaze o Tsurete\" by Miku Itou, Kotori Koiwai, Momo Asakura, Rie Murakawa and Atsuki Nakamura",
-    videoId: "fDkqJ2TP470",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "ED"
-      ]
-    }
-  },
-  {
-    name: "The iDOLM@STER Cinderella Girls06 - \"Step!\" by Sayuri Hara",
-    videoId: "HAHJUtSR90g",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "ED"
-      ]
-    }
-  },
-  {
-    name: "The iDOLM@STER Million Live!10 - \"Thank You!\" by MILLIONSTARS",
-    videoId: "https://ladist1.catbox.video/9fw1sb.webm",
+    name: "3 -  ",
+    videoId: "https://files.catbox.moe/jrrruf.mp3",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
@@ -290,38 +30,8 @@ export const animeEndings = [
     }
   },
   {
-    name: "The iDOLM@STER Shiny Colors07 - \"Tokosaki no Niwa\" by Wakana Maruoka",
-    videoId: "ApxRnZcZhAI",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "ED"
-      ]
-    }
-  },
-  {
-    name: "The iDOLM@STER Cinderella Girls07 - \"Trinity Field\" by Triad Primus",
-    videoId: "WzcK3YIpYtA",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "ED"
-      ]
-    }
-  },
-  {
-    name: "THE IDOLM@STER04 - \"VOY@GER\" by THE IDOLM@STER FIVE STARS!!!!!",
-    videoId: "HJKqSYK_z0w",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "ED"
-      ]
-    }
-  },
-  {
-    name: "The iDOLM@STER Shiny Colors08 - \"Yumesaki After school\" by Houkago Climax Girls",
-    videoId: "https://ladist1.catbox.video/qpwasl.webm",
+    name: "4 -  ",
+    videoId: "https://files.catbox.moe/2bprzg.mp3",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
@@ -330,9 +40,329 @@ export const animeEndings = [
     }
   },
   {
-    name: "The iDOLM@STER Cinderella Girls08 - \"\u304a\u306d\u3060\u308a Shall We\uff5e\uff1f\" by Natsumi Takamori",
-    videoId: "GsSrWdDBVD4",
-    videoSource: VIDEOSOURCE.YOUTUBE,
+    name: "5 -  ",
+    videoId: "https://files.catbox.moe/5v72hm.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "6 -  ",
+    videoId: "https://files.catbox.moe/76wirk.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "7 -  ",
+    videoId: "https://files.catbox.moe/f31wah.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "8 -  ",
+    videoId: "https://files.catbox.moe/k2m673.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "9 -  ",
+    videoId: "https://files.catbox.moe/aqxvsj.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "10 -  ",
+    videoId: "https://files.catbox.moe/r2bagg.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "11 -  ",
+    videoId: "https://files.catbox.moe/tw6ge0.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "12 -  ",
+    videoId: "https://files.catbox.moe/g5ok45.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "13 -  ",
+    videoId: "https://files.catbox.moe/uuiewv.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "14 -  ",
+    videoId: "https://files.catbox.moe/5rh145.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "15 -  ",
+    videoId: "https://files.catbox.moe/6gs1lw.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "16 -  ",
+    videoId: "https://files.catbox.moe/2ffadf.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "17 -  ",
+    videoId: "https://files.catbox.moe/8xozen.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "18 -  ",
+    videoId: "https://files.catbox.moe/e54k8t.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "19 -  ",
+    videoId: "https://files.catbox.moe/2wrh8e.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "20 -  ",
+    videoId: "https://files.catbox.moe/eit4h7.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "21 -  ",
+    videoId: "https://files.catbox.moe/rswz31.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "22 -  ",
+    videoId: "https://files.catbox.moe/wl7skj.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "23 -  ",
+    videoId: "https://files.catbox.moe/9zqpjr.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "24 -  ",
+    videoId: "https://files.catbox.moe/lfvzxg.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "25 -  ",
+    videoId: "https://files.catbox.moe/lcg450.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "26 -  ",
+    videoId: "https://files.catbox.moe/3rps2v.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "27 -  ",
+    videoId: "https://files.catbox.moe/gztys2.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "28 -  ",
+    videoId: "https://files.catbox.moe/rz9gx4.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "29 -  ",
+    videoId: "https://files.catbox.moe/tv3one.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "30 -  ",
+    videoId: "https://files.catbox.moe/vq7ahh.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "31 -  ",
+    videoId: "https://files.catbox.moe/rnxvt9.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "32 -  ",
+    videoId: "https://files.catbox.moe/cqudw8.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "33 -  ",
+    videoId: "https://files.catbox.moe/yymcvg.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "34 -  ",
+    videoId: "https://files.catbox.moe/tupzhh.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "35 -  ",
+    videoId: "https://files.catbox.moe/0z8den.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "36 -  ",
+    videoId: "https://files.catbox.moe/7ltynb.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "ED"
+      ]
+    }
+  },
+  {
+    name: "37 -  ",
+    videoId: "https://files.catbox.moe/eknyv6.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
         "ED"
